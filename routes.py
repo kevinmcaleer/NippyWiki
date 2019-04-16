56543b32d077af2f3e3dffe4c7ceae232178715b
Kevin McAleer <kevinmcaleer@me.com>
2019-04-16 09:35:01 +0100

    removed the venv

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 """ routes.py - website rounting environe for NippyWiki """
 import os
 from flask import Flask, render_template, request, g, jsonify, make_response
+from flask_bootstrap import Bootstrap
 # from models import db, User, Product
 # from forms import SignupForm, AddProductForm
 
@@ -10,6 +11,21 @@ from flask import Flask, render_template, request, g, jsonify, make_response
 APP = Flask(__name__)
 APP.secret_key = "development-key"
 
+def shutdown_server():
+    """ shutsdown the SMARSLab web server """
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@APP.route('/shutdown')
+def shutdown():
+    """ requests the web server shutsdown """
+    shutdown_server()
+    return 'NippyWiki shutting down... Done.'
+
+
 @APP.route("/")
 def index():
     """ default page """
@@ -17,4 +33,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
+    Bootstrap(APP)
     APP.run(debug=True)
