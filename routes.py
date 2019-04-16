c2b500e392df4383e78c40bcbf84900e6cf69bf0
Kevin McAleer <kevinmcaleer@me.com>
2019-04-16 08:57:14 +0100

    Added .gitignore, templates, routes, main.css and handlebars.js (v4.1.2)

--- routes.py
+++ routes.py
@@ -1,15 +1,20 @@
+""" routes.py - website rounting environe for NippyWiki """
+import os
 from flask import Flask, render_template, request, g, jsonify, make_response
 # from models import db, User, Product
 # from forms import SignupForm, AddProductForm
-import os
+
 
 # NippyWiki - a simple python based wiki using markdown
 
-app = Flask(__name__)
-app.secret_key = "development-key"
+APP = Flask(__name__)
+APP.secret_key = "development-key"
 
-@app.route("/")
+@APP.route("/")
 def index():
     """ default page """
     # print("Rendering.index.html")
     return render_template("index.html")
+
+if __name__ == "__main__":
+    APP.run(debug=True)
